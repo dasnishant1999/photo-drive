306703f0437400652e066e812cb02a07e8fb2096
dasnishant1999 <dasnishant1999@gmail.com>
2021-04-12 10:53:49 +0530

    completed:firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+import "firebase/storage";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBZqHV0qLfR0y4ANd2M7ja7V4q_FHZKygo",
+  authDomain: "photo-drive-1914e.firebaseapp.com",
+  projectId: "photo-drive-1914e",
+  storageBucket: "photo-drive-1914e.appspot.com",
+  messagingSenderId: "992857647983",
+  appId: "1:992857647983:web:ee2da092e886ce8227169a",
+  measurementId: "G-VGGJRT18RY",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const photoStorage = firebase.storage();
+const db = firebase.firestore();
+
+export { photoStorage, db };
